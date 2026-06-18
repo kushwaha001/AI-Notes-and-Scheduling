ba7025b11fbada51d089b78e8e5d1e15926760c4
kushagratyagi48-dev <kushagratyagi48@gmail,com>
2026-06-18 12:19:57 +0530

    Integrate Schedule-X calendar component

--- front-end/src/components/CalendarContainer.jsx
+++ front-end/src/components/CalendarContainer.jsx
@@ -1,10 +1,33 @@
+import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import {
+  createViewDay,
+  createViewWeek,
+  createViewMonthGrid,
+} from "@schedule-x/calendar";
+
+import { Temporal } from "temporal-polyfill";
+import "temporal-polyfill/global";
+import "@schedule-x/theme-default/dist/index.css";
+
 function CalendarContainer() {
-  return (
-    <div>
-      <h2>Calendar Component</h2>
-      <p>Schedule-X will be rendered here.</p>
-    </div>
-  );
+  const calendar = useCalendarApp({
+    views: [
+      createViewDay(),
+      createViewWeek(),
+      createViewMonthGrid(),
+    ],
+
+    events: [
+      {
+        id: "1",
+        title: "Project Review",
+        start: Temporal.PlainDate.from("2026-06-20"),
+        end: Temporal.PlainDate.from("2026-06-20"),
+      },
+    ],
+  });
+
+  return <ScheduleXCalendar calendarApp={calendar} />;
 }
 
 export default CalendarContainer;
